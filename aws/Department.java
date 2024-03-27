61cb23249d6c1e13d9d53e66b6f1a2ec29c930e6
T.GNANESWAR <106004033+Gnaneswartunga@users.noreply.github.com>
2024-03-27 19:48:13 +0530

    Create Department.java

---
+++ aws/Department.java
@@ -0,0 +1,18 @@
+package com.awsconnection;
+
+public class Department {
+
+	int id;
+	String name;
+	public Department(int id,String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
